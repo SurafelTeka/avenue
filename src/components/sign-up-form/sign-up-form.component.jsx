e482d34592ff2853dbcd5afa4746aa3e332ead95
Surafel Teka <se.surafel.teka@gmail.com>
2024-05-01 23:01:37 +0300

    button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -7,6 +7,8 @@ import {
 
 import FormInput from "../form-input/form-input.component";
 import { Form } from "react-router-dom";
+import "./sign-up-form.styles.scss";
+import Button from "../button/button.component";
 
 const defaultformFields = {
   displayName: "",
@@ -55,8 +57,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -96,7 +99,7 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
