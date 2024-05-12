407d79848be12c309e5e7f26ab54f482f56417db
Surafel Teka <se.surafel.teka@gmail.com>
2024-05-12 22:04:33 +0300

    cart  context, shopping icon, cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,11 +7,14 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+
+  const { isCartOpen } = useContext(CartContext);
   //console.log("my current user ", currentUser);
 
   return (
@@ -38,7 +41,7 @@ const Navigation = () => {
           <CartIcon />
         </div>
 
-        <CartDropdown />
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
