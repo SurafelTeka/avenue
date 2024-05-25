a055027babac467c0cb7cb55552896da4579e053
Surafel Teka <se.surafel.teka@gmail.com>
2024-05-25 21:18:39 +0300

    batch upload to firestore

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,3 @@
-import SHOP_DATA from "../../shop-data.json";
-
 import { useContext } from "react";
 import { ProductsContext } from "../../contexts/products.context";
 import ProductCard from "../../components/product-card/product-card.component";
