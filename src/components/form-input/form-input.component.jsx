80c064074efb22f22af413f3fae9c80fef1362b1
Surafel Teka <se.surafel.teka@gmail.com>
2024-05-27 14:26:17 +0300

    form input fixed to styled component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,17 +1,13 @@
-import "./form-input.styles.scss";
+import { FormInputLabel, Input, Group } from "./form-input.styles.jsx";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div className="group">
-      <input className="form-input" {...otherProps} />
+    <Group>
+      <Input {...otherProps} />
       {label && (
-        <label
-          className={`${
-            otherProps.value.length ? "shrink" : ""
-          } form-input-label`}
-        >
+        <FormInputLabel shrink={otherProps.value.length}>
           {label}
-        </label>
+        </FormInputLabel>
       )}
       {/* <input className="form-input" {...otherProps} />
         
@@ -20,7 +16,7 @@ const FormInput = ({ label, ...otherProps }) => {
         so placed it above
         
         */}
-    </div>
+    </Group>
   );
 };
 
